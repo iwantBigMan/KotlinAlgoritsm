53907a53629bf601ded6bb009b25c1e958970483
N.K.Hyeon <skarndgus8899@gmail.com>
2022-12-06 01:02:47 +0900

    프로그래머스 : 명예의 전당

---
+++ src/kotlinTest/stringHandling/HallOfFame.kt
@@ -0,0 +1,30 @@
+package kotlinTest.stringHandling
+
+import java.util.*
+
+class HallOfFame {
+    fun solution(k: Int, score: IntArray): IntArray {
+
+        var answer: IntArray = IntArray(score.size)
+        val minScore : PriorityQueue<Int> = PriorityQueue()
+
+        for(i in score.indices){
+            minScore.add(score[i])
+            if(minScore.size > k) minScore.poll()
+            answer[i] = minScore.peek()
+        }
+        return answer
+    }
+}
+
+fun main(){
+    val result = HallOfFame()
+    result.solution(3, intArrayOf(10, 10, 10, 20, 20, 100, 100))
+}
+
+//class Solution {
+//    fun solution(k: Int, score: IntArray): IntArray {
+//        var answer: IntArray = intArrayOf()
+//        return answer
+//    }
+//}
