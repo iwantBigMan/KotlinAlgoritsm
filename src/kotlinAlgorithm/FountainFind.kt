651f5bcae84c91d567e8f28c23f8bd5d869602fb
N.K.Hyeon <skarndgus8899@gmail.com>
2022-12-18 06:00:59 +0900

    코틀린 알고리즘 풀이

--- src/kotlinAlgorithm/FountainFind.kt
+++ src/kotlinAlgorithm/FountainFind.kt
@@ -1,27 +1,24 @@
 package kotlinAlgorithm
 
-import java.util.Scanner
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 // 백준 1193 분수 찾기
 
-fun main() = with(Scanner(System.`in`)){
-    val findNumber = this.nextInt()
-    var sum = 1 // 분모 분자 합
-    var tmp = 0 // 마지막 순서
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    val n = this.readLine().toInt()
 
-    while (tmp < findNumber){
-        tmp += sum
-        sum++
+    var i = 0
+    var sum = 0
+    while (i + sum < n) {
+        sum += i++
     }
 
-    if (sum % 2 == 0){
-        val molecule = (tmp - findNumber + 1)
-        val denominator = sum - molecule
-        println("$molecule / $denominator")
-    }
-    else {
-        val  denominator = (tmp - findNumber + 1)
-        val molecule= sum - denominator
-        println("$molecule / $denominator")
-    }
+    var a = if (i % 2 == 0) n - sum else i + 1 - (n - sum)
+    var b = i + 1 - a
+
+    print("$a/$b")
 }
+
+
+
