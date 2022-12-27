1496de67be289b74da4cfa72a029cd2fe7806bb5
남궁현 <101164731+iwantBigMan@users.noreply.github.com>
2022-12-27 13:55:25 +0900

    백준 10757번 BigInteger 덧셈

--- src/kotlinAlgorithm/IntegerAdditon.kt
+++ src/kotlinAlgorithm/IntegerAdditon.kt
@@ -3,10 +3,10 @@ package kotlinAlgorithm
 
 import java.util.Scanner
 // 백준 10757번
-fun main() = with(Scanner(System.`in`)){
+fun main(): Unit = with(Scanner(System.`in`)){
     val a = this.nextBigInteger()
     val b = this.nextBigInteger()
-    val result = a.plus(b)
+    val result = a.add(b)
     println(result)
 
 }
